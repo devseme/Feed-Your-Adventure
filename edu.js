aabacff644bd89568a86ece3fdca34945f65f8f3
emelda <YOUR EMAIL ADDRESS>
2021-09-22 23:14:36 +0300

    add an event listener function to prevent the form from being submitted automatically.

--- edu.js
+++ edu.js
@@ -1,19 +1,19 @@
 // var eduname=document.getElementById("name")
 // var eduemail=document.getElementById("email")
 var form=document.getElementById("contactform")
-// var eduErrors=document.getElementById(eduerror)
+var eduErrors=document.getElementById(eduerror)
 
-// form.addEventListener("submit",(e)=>{
-//     var errorName=[]
-//     if(eduname.value===''||eduname.value==null){
-//         errorName.push('Please enter your name')
-//     }
-//     if(errorName.length>0){
-//         e.preventDefault()
-//         eduErrors.innertext = errorName.join(",")
+form.addEventListener("submit",(e)=>{
+    var errorName=[]
+    if(eduname.value===''||eduname.value==null){
+        errorName.push('Please enter your name')
+    }
+    if(errorName.length>0){
+        e.preventDefault()
+        eduErrors.innertext = errorName.join(",")
 
-//     }
-// })
+    }
+})
 $(document).ready(function () {
     $("form#contactform").submit(function () {
         var eduName = $("input#name").val();
