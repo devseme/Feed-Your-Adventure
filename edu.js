8ac56c8b7f38168b51672ea6b8ca2e7f2b982f86
emelda <YOUR EMAIL ADDRESS>
2021-09-22 21:44:02 +0300

    create an addEventKistener function to catch errors on the form befor it's submitted.

--- edu.js
+++ edu.js
@@ -2,3 +2,6 @@ var eduname=document.getElementById("name")
 var eduemail=document.getElementById("email")
 var eduform=document.getElementById("contactform")
 
+eduform.addEventListener("submit",(e)=>{
+    e.preventDefault()
+})
