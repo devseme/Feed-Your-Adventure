a2969a1efeba1e313101792fa3712655d43e8c08
emelda <YOUR EMAIL ADDRESS>
2021-09-22 23:07:31 +0300

    Add javascripf functon to make the form functional.

--- edu.js
+++ edu.js
@@ -1,7 +1,30 @@
-var eduname=document.getElementById("name")
-var eduemail=document.getElementById("email")
-var eduform=document.getElementById("contactform")
+// var eduname=document.getElementById("name")
+// var eduemail=document.getElementById("email")
+var form=document.getElementById("contactform")
+// var eduErrors=document.getElementById(eduerror)
 
-eduform.addEventListener("submit",(e)=>{
-    e.preventDefault()
-})
+// form.addEventListener("submit",(e)=>{
+//     var errorName=[]
+//     if(eduname.value===''||eduname.value==null){
+//         errorName.push('Please enter your name')
+//     }
+//     if(errorName.length>0){
+//         e.preventDefault()
+//         eduErrors.innertext = errorName.join(",")
+
+//     }
+// })
+
+$(document).ready(function () {
+    $("form#contactform").submit(function () {
+        var eduName = $("input#name").val();
+        var eduEmail = $("input#email").val();
+        var eduField = $("input#field").val();
+        if ($("input#name").val() && $("input#email").val()) {
+            alert("Hello " + eduName + " Thankyou for your subscription. You will receive a notification whenever there is an intake for "+ eduField);
+        }
+        else {
+            alert("Please enter a valid Name and Email.");
+        }
+    });
+}); 
